9789d9311d0ee0247686f2a20e2c7e4c25790aea
Micha Niskin <asdf@ubergibson.com>
2013-10-18 13:22:38 -0400

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/extype "0.1.0-SNAPSHOT"
+(defproject tailrecursion/extype "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
